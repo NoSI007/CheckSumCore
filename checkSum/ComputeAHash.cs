1e5d78b38db156c6d0cfe431df54b6065d06e638
Khaled Alwan <khaled_alwan@hotmail.com>
2019-05-23 17:34:27 +0100

    added doctored version of CheckSum. Only RIPEMD160 is missing.

---
+++ checkSum/ComputeAHash.cs
@@ -0,0 +1,99 @@
+﻿// (c) Khaled A Alwan .
+// All other rights reserved.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography;
+using System.Threading;
+
+
+namespace checkSum
+{
+    public class ComputeAHash
+    {
+        public ComputeAHash() { }
+
+        public string Hashvalue = null;
+
+        public void MD5(string fname)//TODO: check for null fname
+        {
+            if (string.IsNullOrWhiteSpace(fname))
+                return;
+            StreamReader sr = new StreamReader(fname);
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            byte[] cmphash = md5.ComputeHash(sr.BaseStream);
+            Hashvalue = No2Hex(cmphash);
+        }
+
+        public void RIPEMD160(string fname)//skip for now.
+        {
+            //if (string.IsNullOrWhiteSpace(fname))
+            //    return;
+            //StreamReader sr = new StreamReader(fname);
+            //RIPEMD160 ripmd160 = RIPEMD160Managed.Create();
+            //byte[] cmphash = ripmd160.ComputeHash(sr.BaseStream);
+            //Hashvalue = No2Hex(cmphash);
+        }
+
+        public void SHA1(string fname)//TODO: check for null fname
+        {
+            if (string.IsNullOrWhiteSpace(fname))
+                return;
+            StreamReader sr = new StreamReader(fname);
+            SHA1CryptoServiceProvider sha1 = new SHA1CryptoServiceProvider();
+            byte[] cmphash = sha1.ComputeHash(sr.BaseStream);
+            Hashvalue = No2Hex(cmphash);
+        }
+        public void SHA256(string fname)//TODO: check for null fname
+        {
+            if (string.IsNullOrWhiteSpace(fname))
+                return;
+            StreamReader sr = new StreamReader(fname);
+
+            SHA256CryptoServiceProvider sha256 = new SHA256CryptoServiceProvider();
+            byte[] cmphash = sha256.ComputeHash(sr.BaseStream);
+            Hashvalue = No2Hex(cmphash);
+        }
+
+        public void SHA384(string fname)//TODO: check for null fname
+        {
+            if (string.IsNullOrWhiteSpace(fname))
+                return;
+            StreamReader sr = new StreamReader(fname);
+
+            SHA384CryptoServiceProvider sha384 = new SHA384CryptoServiceProvider();
+            byte[] cmphash = sha384.ComputeHash(sr.BaseStream);
+            Hashvalue = No2Hex(cmphash);
+        }
+
+        public void SHA512(string fname)//TODO: check for null fname
+        {
+            if (string.IsNullOrWhiteSpace(fname))
+                return;
+            StreamReader sr = new StreamReader(fname);
+
+            SHA512CryptoServiceProvider sha512 = new SHA512CryptoServiceProvider();
+            byte[] cmphash = sha512.ComputeHash(sr.BaseStream);
+            Hashvalue = No2Hex(cmphash);
+        }
+
+        string No2Hex(byte[] cmphash)
+        {
+            if (cmphash == null)
+                return string.Empty;
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < cmphash.Length; i++)
+            {
+                sb.Append(cmphash[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+
+
+
+
+    }
+}
---
+++ checkSum/MainWindow.xaml.cs
@@ -0,0 +1,168 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace checkSum
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+
+        public int RESULT
+        {
+            get { return (int)GetValue(RESULTProperty); }
+            set { SetValue(RESULTProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for RESULT.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty RESULTProperty =
+            DependencyProperty.Register("RESULT", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+
+
+        ComputeAHash ComputedHASH = new ComputeAHash();
+        BackgroundWorker bg1 = new BackgroundWorker();
+
+        OpenFileDialog ofd = new OpenFileDialog();
+
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            init();
+        }
+
+
+
+        private void init()
+        {
+            RESULT = 0;
+            DataContext = this;
+            bg1.DoWork += new DoWorkEventHandler(bg1_DoWork);
+            bg1.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bg1_RunWorkerCompleted);
+        }
+
+        void bg1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            z_computed.Text = ComputedHASH.Hashvalue;
+            //if (string.IsNullOrWhiteSpace(this.z_expected.Text) == false )
+            ShowResult();
+            showbtns();
+        }
+
+        private void showbtns()
+        {
+            this.z_open.Visibility = System.Windows.Visibility.Visible;
+            this.Compute.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        void bg1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            switch (e.Argument.ToString())
+            {
+                case "SHA1":
+                    ComputedHASH.SHA1(ofd.FileName);
+                    break;
+                case "MD5":
+                    ComputedHASH.MD5(ofd.FileName);
+                    break;
+                case "RIPEMD160"://skip for now.
+                    //ComputedHASH.RIPEMD160(ofd.FileName);
+                    break;
+                case "SHA512":
+                    ComputedHASH.SHA512(ofd.FileName);
+                    break;
+                case "SHA256":
+                    ComputedHASH.SHA256(ofd.FileName);
+                    break;
+                case "SHA384":
+                    ComputedHASH.SHA384(ofd.FileName);
+                    break;
+            }
+        }
+
+
+
+
+
+        private void z_open_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFile();
+
+        }
+
+        private void OpenFile()
+        {
+            ofd.Multiselect = false;
+            if (ofd.ShowDialog(this) != false)
+            {
+                z_file.Text = ofd.FileName;
+            }
+            RESULT = 0;
+        }
+
+
+
+        private void ShowResult()
+        {
+            if (string.IsNullOrWhiteSpace(z_expected.Text) == false)
+            {
+                string computed = z_computed.Text.ToLower();
+                string expected = z_expected.Text.ToLower();
+                if (string.Compare(computed, expected) == 0)
+                {
+                    RESULT = 1;
+                    return;
+                }
+            }
+
+            RESULT = 999;
+        }
+
+
+
+        private void CompTheHash()
+        {
+
+            bg1.RunWorkerAsync(z_hash.Text);
+            HideBtns();
+        }
+
+        private void HideBtns()
+        {
+            this.z_open.Visibility = System.Windows.Visibility.Hidden;
+            this.Compute.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+
+
+        private void Compute_Click(object sender, RoutedEventArgs e)
+        {
+            CompTheHash();
+        }
+
+
+
+
+
+    }
+}
